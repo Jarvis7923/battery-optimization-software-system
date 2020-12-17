a2775d72e58510fd5214b3eb3ecc385ec8e58c8d
Hablapatabla <lucaslloaiza@gmail.com>
2020-12-16 21:35:21 -0500

    Finish refresh button and clean up some code

--- Final/app/src/main/java/com/group2/boss/MainActivity.java
+++ Final/app/src/main/java/com/group2/boss/MainActivity.java
@@ -51,6 +51,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -228,9 +229,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     ListView listView;
-    ArrayList<String> templateList, uiList;
+    ArrayList<String> uiList;
     ArrayAdapter adapter;
-    Timer myTimer;
 
 //    private static int period = 1000;
     //private int count = 0; //Count is just for debugging purposes to ensure names are being updated
@@ -299,33 +299,18 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         listView = (ListView)findViewById(R.id.AppList);
-        templateList = new ArrayList<>();
         uiList = new ArrayList<>();
-        uiList.add("Temp");
+        uiList.add("Click refresh to load all running processes!");
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, uiList);
 
-        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                System.out.println("Clicked element! ID: " + id + " Position: " + position);
-                Intent intent = new Intent(getApplicationContext(), SpecificAppActivity.class);
-                String name = uiList.get(position);
-                intent.putExtra(SPECIFIC_APP_MESSAGE, name);
-                startActivity(intent);
+        Button refreshButton = (Button)findViewById(R.id.button_id);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                refreshList();
             }
-        });*/
-
-        myTimer = new Timer();
-        myTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                TimerMethod();
-            }
-
-        }, 0, 1000);
+        });
 
         listView.setAdapter(adapter);
-        refreshList();
     }
 
 
@@ -368,7 +353,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void refreshList() {
-        System.out.println("Start refresh list");
         Context ctx = getApplicationContext();
         PackageManager packageManager = ctx.getPackageManager();
         UsageStatsManager usm = (UsageStatsManager) ctx.getSystemService(Context.USAGE_STATS_SERVICE);
@@ -376,10 +360,10 @@ public class MainActivity extends AppCompatActivity {
         long endTime = calendar.getTimeInMillis();
         calendar.add(Calendar.YEAR, -1);
         long startTime = calendar.getTimeInMillis();
-
         Map<String, UsageStats> stats = usm.queryAndAggregateUsageStats(startTime, endTime);
         List<ApplicationInfo> apps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
-
+        adapter.clear();
+        uiList.clear();
         for (ApplicationInfo app : apps) {
             if (stats.containsKey(app.packageName) && !((String)packageManager.getApplicationLabel(app)).startsWith("com.")) {
                 String name = (String)packageManager.getApplicationLabel(app);
@@ -396,119 +380,8 @@ public class MainActivity extends AppCompatActivity {
             Collections.sort(uiList, Collections.reverseOrder());
         }
 
-        for (String name : uiList) {
-            System.out.println("Name: " + name);
-        }
-
-        System.out.println("End refersh list");
+        adapter.notifyDataSetChanged();
     }
-
-    private void refreshTemplateList() {
-        Context ctx = getApplicationContext();
-        PackageManager packageManager = ctx.getPackageManager();
-        UsageStatsManager usm = (UsageStatsManager) ctx.getSystemService(Context.USAGE_STATS_SERVICE);
-        Calendar calendar = Calendar.getInstance();
-        long endTime = calendar.getTimeInMillis();
-        calendar.add(Calendar.YEAR, -1);
-        long startTime = calendar.getTimeInMillis();
-
-        UsageEvents uEvents = usm.queryEvents(startTime, endTime);
-        templateList.clear();
-        while (uEvents.hasNextEvent()) {
-            // https://stackoverflow.com/questions/21215152/how-to-get-names-of-background-running-apps-in-android
-            UsageEvents.Event e = new UsageEvents.Event();
-            uEvents.getNextEvent(e);
-            String packageName = e.getPackageName();
-            try {
-                String name = packageManager.getApplicationInfo(packageName, 0).loadLabel(packageManager).toString();
-                templateList.add(name);
-            } catch (NameNotFoundException nameNotFoundException) {
-
-            }
-        }
-        Set<String> set = new LinkedHashSet<>();
-        set.addAll(templateList);
-        templateList.clear();
-        templateList.addAll(set);
-        ListIterator<String> iter = templateList.listIterator();
-        while (iter.hasNext()) {
-            if (iter.next().startsWith("com.")) {
-                iter.remove();
-            }
-        }
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
-            Boolean switchPref = sharedPref.getBoolean
-                    (SettingsActivity.KEY_PREF_SWITCH_1, false);
-            String thresholdPref = sharedPref.getString(SettingsActivity.KEY_PREF_THRESHOLD_1, null);
-            if (switchPref) {
-                Collections.sort(templateList);
-            } else {
-                // Sort alphabetically descending
-                // Conditional just to not add too much noise to logs
-                //if (count % 5 == 0)
-                //    System.out.println("Descending pref");
-                Collections.sort(templateList, Collections.reverseOrder());
-            }
-    }
-
-    private int getBatteryLevel() {
-        Context ctx = getApplicationContext();
-        BatteryManager bm = (BatteryManager)ctx.getSystemService(BATTERY_SERVICE);
-//        TextView is_charging_view = (TextView)findViewById(R.id.is_charging);
-//        TextView time_left_view = (TextView)findViewById(R.id.timeToCharge);
-        return bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-    }
-
-    private void TimerMethod() {
-        this.refreshTemplateList();
-//        batteryLevel = getBatteryLevel();
-//        this.runOnUiThread(Update_Battery_Charging_Status);
-        this.runOnUiThread(Update_List);
-    }
-
-    private Runnable Update_Battery_Charging_Status = new Runnable() {
-        @Override
-        public void run() {
-            Context ctx = getApplicationContext();
-//            BatteryManager bm = (BatteryManager)ctx.getSystemService(BATTERY_SERVICE);
-//            TextView is_charging_view = (TextView)findViewById(R.id.is_charging);
-//            TextView time_left_view = (TextView)findViewById(R.id.timeToCharge);
-//            int batLevel = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-//
-//            System.out.println("BATLEVEL: " + batLevel);
-//            ProgressBar progressBar = findViewById(R.id.determinateBar);
-//            progressBar.setProgress(batteryLevel);
-////
-//
-            //.addLast(++t, batteryLevel);
-            //plot.redraw();
-//
-//            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
-//            int battery_threshold_pref;
-//            try {
-//                battery_threshold_pref = Integer.parseInt(sharedPref.getString(SettingsActivity.KEY_PREF_THRESHOLD_1, null));
-//            }
-//            catch (NumberFormatException e) {
-//                battery_threshold_pref = 0;
-//            }
-//
-//            if (batLevel <= battery_threshold_pref) {
-//                System.out.println("BELOW THRESHOLD!!!");
-//                notifyUser(ctx);
-//            }
-//
-
-        }
-    };
-
-    private final Runnable Update_List = new Runnable() {
-        public void run() {
-            adapter.clear();
-            uiList.clear();
-            uiList.addAll(templateList);
-            adapter.notifyDataSetChanged();
-        }
-    };
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void notifyUser(Context ctx) {
@@ -559,236 +432,4 @@ public class MainActivity extends AppCompatActivity {
         List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,startTime,endTime);
         return usageStatsList;
     }
-
-    private static boolean isForeground(Context ctx, String myPackage){
-        ActivityManager manager = (ActivityManager) ctx.getSystemService(ACTIVITY_SERVICE);
-        List<ActivityManager.RunningTaskInfo> runningTaskInfo = manager.getRunningTasks(1);
-
-        ComponentName componentInfo = runningTaskInfo.get(0).topActivity;
-        if(componentInfo.getPackageName().equals(myPackage)) {
-            return true;
-        }
-        return false;
-    }
 }
-
-//public class MainActivity extends AppCompatActivity {
-//
-//    // redraws a plot whenever an update is received:
-//    private class MyPlotUpdater implements Observer {
-//        Plot plot;
-//
-//        public MyPlotUpdater(Plot plot) {
-//            this.plot = plot;
-//        }
-//
-//        @Override
-//        public void update(Observable o, Object arg) {
-//            plot.redraw();
-//        }
-//    }
-//
-//    private XYPlot dynamicPlot;
-//    private MyPlotUpdater plotUpdater;
-//    SampleDynamicXYDatasource data;
-//    private Thread myThread;
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//
-//        // android boilerplate stuff
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        // get handles to our View defined in layout.xml:
-//        dynamicPlot = (XYPlot) findViewById(R.id.plot);
-//
-//        plotUpdater = new MyPlotUpdater(dynamicPlot);
-//
-//        // only display whole numbers in domain labels
-//        dynamicPlot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).
-//                setFormat(new DecimalFormat("0"));
-//
-//        // getInstance and position datasets:
-//        data = new SampleDynamicXYDatasource();
-//        SampleDynamicSeries sine1Series = new SampleDynamicSeries(data, 0, "Sine 1");
-//        SampleDynamicSeries sine2Series = new SampleDynamicSeries(data, 1, "Sine 2");
-//
-//        LineAndPointFormatter formatter1 = new LineAndPointFormatter(
-//                Color.rgb(0, 200, 0), null, null, null);
-//        formatter1.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
-//        formatter1.getLinePaint().setStrokeWidth(10);
-//        dynamicPlot.addSeries(sine1Series,
-//                formatter1);
-//
-//        LineAndPointFormatter formatter2 =
-//                new LineAndPointFormatter(Color.rgb(0, 0, 200), null, null, null);
-//        formatter2.getLinePaint().setStrokeWidth(10);
-//        formatter2.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
-//
-//        //formatter2.getFillPaint().setAlpha(220);
-//        dynamicPlot.addSeries(sine2Series, formatter2);
-//
-//        // hook up the plotUpdater to the data model:
-//        data.addObserver(plotUpdater);
-//
-//        // thin out domain tick labels so they dont overlap each other:
-//        dynamicPlot.setDomainStepMode(StepMode.INCREMENT_BY_VAL);
-//        dynamicPlot.setDomainStepValue(5);
-//
-//        dynamicPlot.setRangeStepMode(StepMode.INCREMENT_BY_VAL);
-//        dynamicPlot.setRangeStepValue(10);
-//
-//        dynamicPlot.getGraph().getLineLabelStyle(
-//                XYGraphWidget.Edge.LEFT).setFormat(new DecimalFormat("###.#"));
-//
-//        // uncomment this line to freeze the range boundaries:
-//        dynamicPlot.setRangeBoundaries(-100, 100, BoundaryMode.FIXED);
-//
-//        // create a dash effect for domain and range grid lines:
-//        DashPathEffect dashFx = new DashPathEffect(
-//                new float[] {PixelUtils.dpToPix(3), PixelUtils.dpToPix(3)}, 0);
-//        dynamicPlot.getGraph().getDomainGridLinePaint().setPathEffect(dashFx);
-//        dynamicPlot.getGraph().getRangeGridLinePaint().setPathEffect(dashFx);
-//    }
-//
-//    @Override
-//    public void onResume() {
-//        // kick off the data generating thread:
-//        myThread = new Thread(data);
-//        myThread.start();
-//        super.onResume();
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        data.stopThread();
-//        super.onPause();
-//    }
-//
-//    class SampleDynamicXYDatasource implements Runnable {
-//
-//        // encapsulates management of the observers watching this datasource for update events:
-//        class MyObservable extends Observable {
-//            @Override
-//            public void notifyObservers() {
-//                setChanged();
-//                super.notifyObservers();
-//            }
-//        }
-//
-//        private static final double FREQUENCY = 5; // larger is lower frequency
-//        private static final int MAX_AMP_SEED = 100;
-//        private static final int MIN_AMP_SEED = 10;
-//        private static final int AMP_STEP = 1;
-//        static final int SINE1 = 0;
-//        static final int SINE2 = 1;
-//        private static final int SAMPLE_SIZE = 31;
-//        private int phase = 0;
-//        private int sinAmp = 1;
-//        private MyObservable notifier;
-//        private boolean keepRunning = false;
-//
-//        {
-//            notifier = new MyObservable();
-//        }
-//
-//        void stopThread() {
-//            keepRunning = false;
-//        }
-//
-//        //@Override
-//        public void run() {
-//            try {
-//                keepRunning = true;
-//                boolean isRising = true;
-//                while (keepRunning) {
-//
-//                    Thread.sleep(10); // decrease or remove to speed up the refresh rate.
-//                    phase++;
-//                    if (sinAmp >= MAX_AMP_SEED) {
-//                        isRising = false;
-//                    } else if (sinAmp <= MIN_AMP_SEED) {
-//                        isRising = true;
-//                    }
-//
-//                    if (isRising) {
-//                        sinAmp += AMP_STEP;
-//                    } else {
-//                        sinAmp -= AMP_STEP;
-//                    }
-//                    notifier.notifyObservers();
-//                }
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        int getItemCount(int series) {
-//            return SAMPLE_SIZE;
-//        }
-//
-//        Number getX(int series, int index) {
-//            if (index >= SAMPLE_SIZE) {
-//                throw new IllegalArgumentException();
-//            }
-//            return index;
-//        }
-//
-//        Number getY(int series, int index) {
-//            if (index >= SAMPLE_SIZE) {
-//                throw new IllegalArgumentException();
-//            }
-//            double angle = (index + (phase))/FREQUENCY;
-//            double amp = sinAmp * Math.sin(angle);
-//            switch (series) {
-//                case SINE1:
-//                    return amp;
-//                case SINE2:
-//                    return -amp;
-//                default:
-//                    throw new IllegalArgumentException();
-//            }
-//        }
-//
-//        void addObserver(Observer observer) {
-//            notifier.addObserver(observer);
-//        }
-//
-//        public void removeObserver(Observer observer) {
-//            notifier.deleteObserver(observer);
-//        }
-//    }
-//
-//    class SampleDynamicSeries implements XYSeries {
-//        private SampleDynamicXYDatasource datasource;
-//        private int seriesIndex;
-//        private String title;
-//
-//        SampleDynamicSeries(SampleDynamicXYDatasource datasource, int seriesIndex, String title) {
-//            this.datasource = datasource;
-//            this.seriesIndex = seriesIndex;
-//            this.title = title;
-//        }
-//
-//        @Override
-//        public String getTitle() {
-//            return title;
-//        }
-//
-//        @Override
-//        public int size() {
-//            return datasource.getItemCount(seriesIndex);
-//        }
-//
-//        @Override
-//        public Number getX(int index) {
-//            return datasource.getX(seriesIndex, index);
-//        }
-//
-//        @Override
-//        public Number getY(int index) {
-//            return datasource.getY(seriesIndex, index);
-//        }
-//    }
-//}
